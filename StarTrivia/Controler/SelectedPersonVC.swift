9d6f8b4c53fb5f12e64dca4e25349c84b5f584cb
Ahmad <eisamaherahmad@gmail.com>
2019-07-18 23:49:27 -0700

    Fade in & out first way

--- StarTrivia/Controler/SelectedPersonVC.swift
+++ StarTrivia/Controler/SelectedPersonVC.swift
@@ -17,8 +17,18 @@ class SelectedPersonVC: UIViewController {
     @IBOutlet weak var hairLbl: UILabel!
     @IBOutlet weak var birthYearLbl: UILabel!
     @IBOutlet weak var genderLbl: UILabel!
+    @IBOutlet weak var homeworldBtn: UIButton!
+    @IBOutlet weak var vehiclesBtn: UIButton!
+    @IBOutlet weak var starshipsBtn: UIButton!
+    @IBOutlet weak var filmsBtn: UIButton!
+    
+    
+    
+    
     
     var personApi = PersonApi()
+   
+    
     
     override func viewDidLoad() {
         
@@ -30,22 +40,31 @@ class SelectedPersonVC: UIViewController {
         let random = Int.random(in: 1...87)
         personApi.getRandomPersonAlamoDecoder(id: random) { (person) -> (Void) in
             if let person = person {
-             setupView(person: person)
+                self.setupView(person: person)
             }
         }
         
-        //update UI Function
-        func setupView(person: Person){
-            nameLbl.text = person.name
-            heightLbl.text = person.height
-            massLbl.text = person.mass
-            hairLbl.text = person.hair
-            birthYearLbl.text = person.birthYear
-            genderLbl.text = person.gender
-    
-    }
+      
     
 }
+    
+    //update UI Function
+    func setupView(person: Person){
+        nameLbl.text = person.name
+        heightLbl.text = person.height
+        massLbl.text = person.mass
+        hairLbl.text = person.hair
+        birthYearLbl.text = person.birthYear
+        genderLbl.text = person.gender
+        // Fade in & out first way 
+        homeworldBtn.isEnabled = !person.homeWorldUrl.isEmpty
+        vehiclesBtn.isEnabled = !person.vechileUrls.isEmpty
+        starshipsBtn.isEnabled = !person.starShipUrls.isEmpty
+        filmsBtn.isEnabled = !person.filmUrls.isEmpty
+        
+    
+        
+    }
 }
 
 
