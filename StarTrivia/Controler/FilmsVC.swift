650605aedb554f4d45be5a677654cee78c62b6b4
Ahmad <eisamaherahmad@gmail.com>
2019-07-19 01:05:41 -0700

    Set Up Additional VC

---
+++ StarTrivia/Controler/FilmsVC.swift
@@ -0,0 +1,21 @@
+//
+//  FilmsVC.swift
+//  StarTrivia
+//
+//  Created by mac on 7/19/19.
+//  Copyright © 2019 maher.eisa. All rights reserved.
+//
+
+import UIKit
+
+class FilmsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
---
+++ StarTrivia/Controler/StarshipsVC.swift
@@ -0,0 +1,21 @@
+//
+//  StarshipsVC.swift
+//  StarTrivia
+//
+//  Created by mac on 7/19/19.
+//  Copyright © 2019 maher.eisa. All rights reserved.
+//
+
+import UIKit
+
+class StarshipsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
