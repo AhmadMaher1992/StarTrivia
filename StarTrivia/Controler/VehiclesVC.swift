00cb7e22e2b7de35d3448f649c29b92fc8d014b1
Ahmad <eisamaherahmad@gmail.com>
2019-07-19 23:10:37 -0700

    Vehicles UI

--- StarTrivia/Controler/VehiclesVC.swift
+++ StarTrivia/Controler/VehiclesVC.swift
@@ -11,6 +11,20 @@ import UIKit
 class VehiclesVC: UIViewController , PersonProtocol{
     var person: Person!
     
+    
+    @IBOutlet weak var nameLBL: UILabel!
+    @IBOutlet weak var modelLBL: UILabel!
+    @IBOutlet weak var makerLBL: UILabel!
+    @IBOutlet weak var costLBL: UILabel!
+    @IBOutlet weak var lengthLBL: UILabel!
+    @IBOutlet weak var speedLBL: UILabel!
+    @IBOutlet weak var crewLBL: UILabel!
+    @IBOutlet weak var passenLBL: UILabel!
+    @IBOutlet weak var previousBtn: UIButton!
+    @IBOutlet weak var nextBtn: UIButton!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -18,5 +32,12 @@ class VehiclesVC: UIViewController , PersonProtocol{
     }
     
 
-
+    @IBAction func previousClicked(_ sender: Any) {
+    }
+    
+    @IBAction func nextClicked(_ sender: Any) {
+    }
+    
+    
+    
 }
