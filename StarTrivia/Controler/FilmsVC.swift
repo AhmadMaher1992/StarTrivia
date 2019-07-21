e192adbe153016c0ac51f97b7c42fe349c234884
Ahmad <eisamaherahmad@gmail.com>
2019-07-21 16:36:28 -0700

    Films View Controller UI

--- StarTrivia/Controler/FilmsVC.swift
+++ StarTrivia/Controler/FilmsVC.swift
@@ -11,6 +11,15 @@ import UIKit
 class FilmsVC: UIViewController , PersonProtocol{
     var person: Person!
     
+    @IBOutlet weak var titleLbl: UILabel!
+    @IBOutlet weak var episodeLbl: UILabel!
+    @IBOutlet weak var directorLbl: UILabel!
+    @IBOutlet weak var producerLbl: UILabel!
+    @IBOutlet weak var releasedLbl: UILabel!
+    @IBOutlet weak var crawlTxtView: UITextView!
+    @IBOutlet weak var previousBtn: UIButton!
+    @IBOutlet weak var nextBtn: UIButton!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +27,10 @@ class FilmsVC: UIViewController , PersonProtocol{
        print(person.name)
     }
     
-
-
+    @IBAction func previousClicked(_ sender: Any) {
+    }
+    
+    @IBAction func nextClicked(_ sender: Any) {
+    }
+    
 }
